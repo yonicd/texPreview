38a6a511f775ad6d4a145242f56aa84d94832d20
Dan Polhamus <danp@metrumrg.com>
2017-01-31 11:04:58 -0500

    Fixed the process of writing out to a temp file

--- R/texPreview.R
+++ R/texPreview.R
@@ -34,59 +34,59 @@
 #' texPreview(obj = x,fileNM = 'eq',imgFormat = 'png')
 
 
-texPreview=function(obj,fileNM,fileDir=NULL,overwrite=T,imgFormat='png',print.xtable.opts=list()){
-
-  if(is.null(fileDir)){
+texPreview <- function (obj, fileNM, fileDir = NULL, overwrite = T, imgFormat = "png", 
+                        print.xtable.opts = list()) 
+{
+  if (is.null(fileDir)) {
     fileDir <- tempdir()
-    if(!dir.exists(fileDir)) dir.create(fileDir,recursive = T)
-    writeFlg=F
-  }else{
-    writeFlg=T
-    if(!dir.exists(fileDir)) return()
+    if (!dir.exists(fileDir)) 
+      dir.create(fileDir, recursive = T)
+    writeFlg = F
   }
-
-  if('xtable'%in%class(obj)){
-    print.xtable.opts$x=obj
-    if(!'file'%in%names(print.xtable.opts)) print.xtable.opts$file=file.path(fileDir,paste0(fileNM,'.tex'))
-    obj=do.call('print',print.xtable.opts)
-  } 
-  
-  cat(paste('\\documentclass[varwidth, border={10 5 50 5}]{standalone}',
-            '\\usepackage[usenames,dvispnames,svgnames,table]{xcolor}',
-            '\\usepackage{multirow}',
-            '\\usepackage{helvet}',
-            '\\usepackage{amsmath}',
-            '\\usepackage{rotating}',
-            '\\usepackage{graphicx}',
-            '\\renewcommand{\\familydefault}{\\sfdefault}',
-            '\\usepackage{setspace}',
-            '\\usepackage{caption}',
-            '\\captionsetup{labelformat=empty}',
-            '\\begin{document}',
-            obj,
-            '\\end{document}'
-            ),
-      file=file.path(fileDir,paste0(fileNM,'Doc.tex'))
-      )
-
-  x=getwd()
+  else {
+    writeFlg = T
+    if (!dir.exists(fileDir)) 
+      return()
+  }
+  if ("xtable" %in% class(obj)) {
+    print.xtable.opts$x = obj
+    if (!"file" %in% names(print.xtable.opts)) 
+      print.xtable.opts$file = file.path(fileDir, paste0(fileNM, 
+                                                         ".tex"))
+    obj = do.call("print", print.xtable.opts)
+  }
+  newobj <- c(
+    "\\documentclass[varwidth, border={10 5 50 5}]{standalone}", 
+    "\\usepackage[usenames,dvispnames,svgnames,table]{xcolor}", 
+    "\\usepackage{multirow}", "\\usepackage{helvet}", "\\usepackage{amsmath}", 
+    "\\usepackage{rotating}", "\\usepackage{graphicx}", 
+    "\\renewcommand{\\familydefault}{\\sfdefault}", "\\usepackage{setspace}", 
+    "\\usepackage{caption}", "\\captionsetup{labelformat=empty}", 
+    "\\begin{document}", obj, "\\end{document}"    
+  )
+  writeLines(newobj, con = file.path(fileDir, paste0(fileNM, "Doc.tex")))
+  x = getwd()
   setwd(fileDir)
-
-  system(paste('pdflatex',
-               file.path(fileDir,paste0(fileNM,'Doc.tex')),
-               file.path(fileDir,paste0(fileNM,'Doc.pdf')),
-               '-halt-on-error'
-  )
-  )
+  system(paste("pdflatex", file.path(fileDir, paste0(fileNM, 
+                                                     "Doc.tex")), file.path(fileDir, paste0(fileNM, "Doc.pdf")), 
+               "-halt-on-error"))
   setwd(x)
-
-  imgOut=image_convert(image = image_read(path = file.path(fileDir,paste0(fileNM,'Doc.pdf')),density = 150),format = imgFormat,depth = 16)
-  cat('\014')  
-  if(writeFlg&overwrite){
-    image_write(imgOut,file.path(fileDir,paste0(fileNM,'.',imgFormat)))
-    if(!'file'%in%names(print.xtable.opts)) print.xtable.opts$file=file.path(fileDir,paste0(fileNM,'.tex'))
-    if('xtable'%in%class(obj)) do.call('print',print.xtable.opts)
+  imgOut = image_convert(image = image_read(path = file.path(fileDir, 
+                                                             paste0(fileNM, "Doc.pdf")), density = 150), format = imgFormat, 
+                         depth = 16)
+  cat("\f")
+  if (writeFlg & overwrite) {
+    image_write(imgOut, file.path(fileDir, paste0(fileNM, 
+                                                  ".", imgFormat)))
+    if (!"file" %in% names(print.xtable.opts)) 
+      print.xtable.opts$file = file.path(fileDir, paste0(fileNM, 
+                                                         ".tex"))
+    if ("xtable" %in% class(obj)) 
+      do.call("print", print.xtable.opts)
   }
   print(imgOut)
-  suppressWarnings({junk=sapply(list.files(file.path(fileDir),pattern = 'Doc'),function(x) file.remove(file.path(fileDir,x)))})
+  suppressWarnings({
+    junk = sapply(list.files(file.path(fileDir), pattern = "Doc"), 
+                  function(x) file.remove(file.path(fileDir, x)))
+  })
 }
