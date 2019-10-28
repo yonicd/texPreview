306c7bf46e8d5423e167095b241779223570680f
Jonathan Sidi <yonis@metrumrg.com>
2019-10-28 06:30:21 -0400

    add skip_on_cran for test_pdf

--- tests/testthat/test-rmd.R
+++ tests/testthat/test-rmd.R
@@ -4,36 +4,9 @@ path <- file.path(tempdir(),'rmd')
 
 cleanup(path)
 
-rmarkdown::render('../assets/input/test_pdf.Rmd',
-                  output_dir = path, params = list(path = path),
-                  quiet = TRUE)
-
 rmarkdown::render('../assets/input/test_html.Rmd',
                   output_dir = path, params = list(path = path),
                   quiet = TRUE)
-
-testthat::describe('pdf',{
-  
-  it('output device', {
-    testthat::expect_true(file.exists(file.path(path,'test_pdf.pdf')))
-  })
-  
-  it('returnType tex', {
-    testthat::expect_true(file.exists(file.path(path,'test_tex.tex')))
-  })
-
-  it('png tex', {
-    testthat::expect_true(file.exists(file.path(path,'test_tex.png')))
-  })
-    
-  it('returnType input', {
-    testthat::expect_true(file.exists(file.path(path,'test_input.tex')))
-  })
-
-  it('png input', {
-    testthat::expect_true(file.exists(file.path(path,'test_input.png')))
-  })
-})
 
 testthat::describe('html',{
   
@@ -45,6 +18,35 @@ testthat::describe('html',{
     testthat::expect_true(file.exists(file.path(path,'test_html.png')))
   })
     
+})
+
+testthat::describe('pdf',{
+
+  testthat::skip_on_cran()
+  
+  rmarkdown::render('../assets/input/test_pdf.Rmd',
+                    output_dir = path, params = list(path = path),
+                    quiet = TRUE)
+    
+  it('output device', {
+    testthat::expect_true(file.exists(file.path(path,'test_pdf.pdf')))
+  })
+  
+  it('returnType tex', {
+    testthat::expect_true(file.exists(file.path(path,'test_tex.tex')))
+  })
+  
+  it('png tex', {
+    testthat::expect_true(file.exists(file.path(path,'test_tex.png')))
+  })
+  
+  it('returnType input', {
+    testthat::expect_true(file.exists(file.path(path,'test_input.tex')))
+  })
+  
+  it('png input', {
+    testthat::expect_true(file.exists(file.path(path,'test_input.png')))
+  })
 })
 
 cleanup(path,create = FALSE)
