9700b03a6e5bd60e650f57d6b38e19967e1ee26f
Jonathan Sidi <yonis@metrumrg.com>
2020-02-13 14:27:49 -0500

    revert skip on cran

--- tests/testthat/test-rmd.R
+++ tests/testthat/test-rmd.R
@@ -20,8 +20,6 @@ testthat::describe('html',{
 
 testthat::describe('pdf',{
 
-  testthat::skip_on_cran()
-  
   safe_render(input = '../assets/input/test_pdf.Rmd',output_dir = path, render_params = list(path = path), quiet = TRUE)
   
   it('output device', {
